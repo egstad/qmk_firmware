e2f02c5edf72cc2e9222435a19a9fe5778b42064
Jordan Egstad <jordanegstad@gmail.com>
2021-01-09 15:52:56 -0800

    updated keymap

--- keyboards/kbdfans/maja/keymaps/egstad/README.md
+++ keyboards/kbdfans/maja/keymaps/egstad/README.md
@@ -1,6 +1,13 @@
 # Egstad
 
-> A custom layout for Vulcan & KBDFans ergonimic mechanical keyboard
+> A PC and Mac friendly keymap for Vulcan & KBDFans ergonimic mechanical keyboard
+
+## Mac / PC Mode
+With the help of [BootMagic Keycode](https://docs.qmk.fm/#/feature_bootmagic?id=keycodes), `CG_TOGG`, we can swap the Command and Control Keys. At the time of this writing, that key can be activated by pressing Space and LGUI together. 
+
+## Auto-Shifting
+Tap a key and you get its character. Tap a key, but hold it slightly longer and you get its shifted state. Voilà! No shift key needed! To toggle this on/off, press and hold Space and Shift together.
+
 
 ## Keeping up to date
 To update your fork’s master branch, run the following, hitting the Enter key after each line:
--- keyboards/kbdfans/maja/keymaps/egstad/keymap.c
+++ keyboards/kbdfans/maja/keymaps/egstad/keymap.c
@@ -58,10 +58,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 			 * █▀░ █▄█ █░▀█ █▄▄ ░█░ █ █▄█ █░▀█
 			 */  
 			RESET,  	KC_F1,    KC_F2,   	KC_F3,  	KC_F4,   	KC_F5,   	KC_F6,   	KC_F7,   	KC_F8,   	KC_F9,   	KC_F10,   KC_F11,  	KC_F12,  	KC_DEL,   KC_TRNS,
-		  KC_ASTG,  RGB_TOG,  RGB_MOD, 	RGB_HUI,	RGB_SAI, 	RGB_VAI, 	KC_TRNS, 	NX_WRD, 	LN_STR, 	LN_END, 	PV_WRD, 	KC_TRNS,  KC_TRNS, 	KC_TRNS,  KC_BRIU,
+		  KC_TRNS,  RGB_TOG,  RGB_MOD, 	RGB_HUI,	RGB_SAI, 	RGB_VAI, 	KC_TRNS, 	NX_WRD, 	LN_STR, 	LN_END, 	PV_WRD, 	KC_TRNS,  KC_TRNS, 	KC_TRNS,  KC_BRIU,
 			KC_TRNS,  SEL_PV,   SEL_ST, 	SEL_EN,		SEL_NX, 	KC_TRNS, 	KC_TRNS, 	KC_LEFT, 	KC_UP,   	KC_DOWN, 	KC_RGHT,  KC_TRNS,          	KC_MPLY,  KC_BRID,
-		  KC_TRNS,  KC_TRNS,  KC_TRNS, 	KC_TRNS,	KC_TRNS, 	KC_TRNS, 	KC_TRNS, 	KC_TRNS, 	KC_TRNS, 	KC_TRNS, 	KC_TRNS,  KC_TRNS, 	KC_MUTE, 	KC_VOLU,
-		  KC_TRNS,            KC_TRNS, 	KC_TRNS,  KC_DEL,           	KC_TRNS,  KC_TRNS,                    											KC_MPRV, 	KC_VOLD,  KC_MNXT),
+		  KC_ASTG,  KC_TRNS,  KC_TRNS, 	KC_TRNS,	KC_TRNS, 	KC_TRNS, 	KC_TRNS, 	KC_TRNS, 	KC_TRNS, 	KC_TRNS, 	KC_TRNS,  KC_TRNS, 	KC_MUTE, 	KC_VOLU,
+		  KC_TRNS,            KC_TRNS, 	CG_TOGG,  KC_DEL,           	KC_TRNS,  KC_TRNS,                    											KC_MPRV, 	KC_VOLD,  KC_MNXT),
 		[2] = LAYOUT(
 			/*
 			 * █▄░█ █░█ █▀▄▀█ █▀█ ▄▀█ █▀▄
